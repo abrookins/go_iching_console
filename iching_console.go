f3f157fc420c98a96db47e6ff6679fdd11bb0868
Andrew Brookins <a.m.brookins@gmail.com>
2012-06-01 16:19:16 -0700

    Fix hexagram comparison, use an `io.Reader` to get input.

--- iching_console.go
+++ iching_console.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-        "fmt"
-        "github.com/abrookins/go_iching"
+	"bufio"
+	"fmt"
+	"github.com/abrookins/go_iching"
+	"os"
 )
 
 // reportHexagram prints details about a hexagram.
 func reportHexagram(hexagram *iching.Hexagram) {
-        fmt.Printf("Result: %v (%v, #%v)\n\nDescription:\n%v\n\nTranslations:\n", 
-                   hexagram.Name, hexagram.Character, hexagram.Num,
-                   hexagram.Description)
+	fmt.Printf("Result: %v (%v, #%v)\n\nDescription:\n%v\n\nTranslations:\n",
+		hexagram.Name, hexagram.Character, hexagram.Num,
+		hexagram.Description)
 
-        for _, tr := range hexagram.TranslationUrls {
-                fmt.Println(tr)
-        }
+	for _, tr := range hexagram.TranslationUrls {
+		fmt.Println(tr)
+	}
 }
 
 func main() {
-        var question string
-        fmt.Println("What is your question for the oracle?\n")
-        fmt.Scanln(&question)
-        reading := iching.GetReading(question)
+	fmt.Println("What is your question for the oracle?\n")
+	
+	in := bufio.NewReader(os.Stdin)
+	question, _ := in.ReadString('\n')
+	reading := iching.GetReading(question)
 
-        fmt.Printf("----------------------------------\n\nLines: %v\n\n", 
-                   reading.Lines)
-        reportHexagram(reading.Hexagram)
+	fmt.Printf("----------------------------------\n\nLines: %v\n\n", reading.Lines)
+	reportHexagram(reading.Hexagram)
 
-        if reading.NextHexagram != reading.Hexagram {
-                fmt.Println("\nHexagram is changing. Next hexagram:")
-                reportHexagram(reading.NextHexagram)
-        }
+	if reading.NextHexagram.Num != reading.Hexagram.Num {
+		fmt.Println("\nHexagram is changing. Next hexagram:")
+		reportHexagram(reading.NextHexagram)
+	}
 }
